69356b6f65f7e60baccc0aa0d641c1beb5cb3504
venkey1185 <venkey1185@gmail.com>
2018-04-11 10:56:02 +0000

    first commit

---
+++ build_cloud.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+export cloud_to_use=gce
+
+source $(dirname "${BASH_SOURCE[0]}")/set_cloud.sh
+
+case $cloud_to_use in
+aws|azure|gce|openstack)
+  ;;
+*)
+  message="CLOUD_TO_USE environment variable was set to \"$CLOUD_TO_USE\" but must be set to one of the following: aws, azure, gce, openstack"
+  echo -e $message
+  exit 1
+  ;;
+esac
+
+ansible-playbook --connection=local "playbooks/clouds/build_${cloud_to_use}.yml"
